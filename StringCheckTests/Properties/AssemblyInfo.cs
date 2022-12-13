d720bf18042a633be6edf72ab7aff28e34539f37
Daimon <118897859+Daimon153@users.noreply.github.com>
2022-12-13 10:45:09 +0500

    Создание 1 теста

---
+++ StringCheckTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringCheckTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringCheckTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec175555-1b90-4bc5-8b5d-a4c6a740fbf2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StringCheckTests/StringCheckTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using StringCheckLibrary;
+using System.Collections.Generic;
+
+namespace StringCheckTests
+{
+    [TestClass]
+    public class StringCheckTest
+    {
+        [TestMethod]
+        public void StringCheckMethod()
+        {
+            //Arrange
+            string text = "Ананас";
+            List<char> expected = new List<char> { 'А', 'Н', 'С' };
+            //Act
+            List<char> actual = StringCheckClass.GetLetters(text);
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
