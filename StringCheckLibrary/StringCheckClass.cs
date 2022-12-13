2e8bb69f28be93bed572417ff01dceaddd2ad8c5
Daimon <118897859+Daimon153@users.noreply.github.com>
2022-12-13 10:27:59 +0500

    Сортировка

--- StringCheckLibrary/StringCheckClass.cs
+++ StringCheckLibrary/StringCheckClass.cs
@@ -11,13 +11,15 @@ namespace StringCheckLibrary
         /// <summary>
         /// Возвращение букв на лист
         /// </summary>
-        /// <param name="textString"></param>
-        /// <returns></returns>
+        /// <param name="textString">
+        /// В качестве параметра передается строка, которая может быть пустой или содержать некоторый текст. Текст может включать в себя произвольные символы.</param>
+        /// <returns>Возвращается список символов List<char>. Список должен содержать буквы искомого текста и удовлетворять требованиям, указанным выше.</returns>
         public static List<char> GetLetters(string textString)
         {
             List<char> list = new List<char>();
+            //заглавные буквы
             textString = textString.ToUpper();
-
+            
             foreach(char c in textString)
             {// проверка на уникальность символа    
                 if(list.Contains(c))
@@ -28,6 +30,9 @@ namespace StringCheckLibrary
                 }
                 
             }
+            //отсортировка
+            list.Sort();
+            //возвращение листа
             return(list);
         }
 
