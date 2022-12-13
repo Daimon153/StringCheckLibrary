0ce878eaf3a17ca9b23162ad5d8ab63837bf99f0
Daimon <118897859+Daimon153@users.noreply.github.com>
2022-12-13 10:00:20 +0500

    Добавьте файлы проекта.

---
+++ StringCheckLibrary/StringCheckClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCheckLibrary
+{
+    public class StringCheckClass
+    {
+        /// <summary>
+        /// Возвращение букв на лист
+        /// </summary>
+        /// <param name="textString"></param>
+        /// <returns></returns>
+        public static List<char> GetLetters(string textString)
+        {
+            List<char> list = new List<char>();
+            return(list);
+        }
+    }
+}
